7ec9295ff607727211780d7949d7e97db322e132
Torbjørn Søiland <tsoiland@avacati.net>
2011-05-03 12:15:25 +1000

    new db schema

--- Aeolus/scripts/schema.sql
+++ Aeolus/scripts/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 19, 2011 at 10:48 AM
+-- Generation Time: May 03, 2011 at 12:14 PM
 -- Server version: 5.1.49
 -- PHP Version: 5.3.3-1ubuntu9.3
 
@@ -28,6 +28,7 @@ USE `aeolus`;
 -- Table structure for table `incidents`
 --
 
+DROP TABLE IF EXISTS `incidents`;
 CREATE TABLE IF NOT EXISTS `incidents` (
   `title` varchar(50) NOT NULL,
   `description` text NOT NULL,
@@ -44,12 +45,28 @@ CREATE TABLE IF NOT EXISTS `incidents` (
 
 INSERT INTO `incidents` (`title`, `description`, `id`, `latitude`, `longitude`, `verified`) VALUES
 ('Testtitle', 'This is the test description', 1, -34.397, 150.644, 1),
-('trdfg', 'sdg', 2, -35.397, 150.644, 1),
-('fdsg', 'sdfg', 3, -33.27, 151.792, 0),
-('hello', 'test', 4, -33.2792, 149.595, 1),
-('rwer', 'wer', 5, -23.0333, 145.969, 1),
-('tertsfg', 'sdg', 6, -24.225, 148.881, 0),
-('wohoo', 'test', 7, -27.6622, 152.924, 0);
+('trdfg', 'sdg', 2, -35.397, 150.644, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_incident`
+--
+
+DROP TABLE IF EXISTS `user_incident`;
+CREATE TABLE IF NOT EXISTS `user_incident` (
+  `user_id` int(11) NOT NULL,
+  `incident_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`incident_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_incident`
+--
+
+INSERT INTO `user_incident` (`user_id`, `incident_id`) VALUES
+(1, 1),
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -57,20 +74,21 @@ INSERT INTO `incidents` (`title`, `description`, `id`, `latitude`, `longitude`, 
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `salt` varchar(50) NOT NULL,
   `role` varchar(50) NOT NULL,
-  `clock_in_time` timestamp NULL DEFAULT NULL,
-  `clock_out_time` timestamp NULL DEFAULT NULL,
+  `clock_in_time` bigint(20) DEFAULT NULL,
+  `clock_out_time` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `salt`, `role`, `clock_in_time`, `clock_out_time`) VALUES
-(1, 'admin', '45d0122d11b30012eb0111b09181712f16620a7a', 'ce8d96d579d389e783f95b3772785783ea1a9854', 'admin', NULL, NULL);
+(1, 'admin', '45d0122d11b30012eb0111b09181712f16620a7a', 'ce8d96d579d389e783f95b3772785783ea1a9854', 'admin', 1304388834, 1304388833);
